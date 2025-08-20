cdea56554eff584a150a8b28edb77da6a14e60a1
mahmo <mahmoudiabdelali@gmail.com>
2025-08-20 17:35:52 +0200

    Impliment API list and retrieve with ModelViewSet for Authors

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import AuteurProfile
+from .models import AuteurProfile,Film
 
 class AuteurListSerializer(serializers.ModelSerializer):
 
@@ -10,3 +10,19 @@ class AuteurListSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuteurProfile
         fields = ("id", "username", "email", "nom", "date_naissance", "source")
+
+class FilmMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Film
+        fields = ("id", "titre", "date_sortie")
+
+class AuteurDetailSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username", read_only=True)
+    email = serializers.EmailField(read_only=True)
+    nom = serializers.CharField(read_only=True)
+
+    films = FilmMiniSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = AuteurProfile
+        fields = ("id", "username", "email", "nom", "date_naissance", "source", "films")
--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,29 @@
 # apps/users/views.py (extrait)
-from rest_framework import viewsets, mixins
-from rest_framework.permissions import AllowAny
-from .models import AuteurProfile
-from .serializers import AuteurListSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny,IsAuthenticated
 from .paginations import SimplePagination
-class AuteurViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+from .models import AuteurProfile
+from .serializers import AuteurListSerializer, AuteurDetailSerializer
+
+class AuteurViewSet(ModelViewSet):
+
     queryset = AuteurProfile.objects.all()
-    serializer_class = AuteurListSerializer
     permission_classes = [AllowAny]
-    pagination_class = SimplePagination
+    http_method_names = ['get', 'head', 'options']
     ordering_fields = ("id",)
+    pagination_class = SimplePagination
+
+    def get_permissions(self):
+        permission_classes = [AllowAny]
+
+        if self.action == 'list':
+            permission_classes =  [AllowAny]
+        elif self.action == 'retrieve':
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return AuteurListSerializer
+        elif self.action == 'retrieve':
+            return  AuteurDetailSerializer
